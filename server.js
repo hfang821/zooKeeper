be029d29cd380cf85e05e16157f92f755581d020
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-04-27 23:26:33 -0400

    adding the POST functionality and validation

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const {animals} = require('./data/animals.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parse incoming string or array data (extend:true means tell our server may be sub-array data nested so need to look deep)
+app.use(express.urlencoded({ extended:true}));
+//parse incoming JSON data into the req.body javascript object
+app.use(express.json());
 
 function filterByQuery(query,animalsArray){
     let personalityTraitsArray = [];
@@ -48,6 +54,39 @@ function findById(id,animalsArray) {
     return result;
 }
 
+function createNewAnimal(body,animalsArray) {
+    console.log(body);
+    //function's main code here
+    const animal = body;
+    animalsArray.push(animal);
+
+    fs.writeFileSync(
+        //the path will be from the root of whatever machine this code runs on to the location of our animals.json file.
+        path.join(__dirname,'./data/animals.json'),
+        //null means we dont want to edit any of our exising data
+        //2 means we want to create white space between our values to make it more readable.
+        JSON.stringify({animals: animalsArray}, null, 2)
+    );
+    //return finished code to post route for response
+    return animal;
+}
+
+function validateAnimal(animal){
+    if(!animal.name || typeof animal.name !=='string'){
+        return false;
+    }
+    if(!animal.species || typeof animal.species !=='string'){
+        return false;
+    }
+    if(!animal.diet || typeof animal.diet !=='string'){
+        return false;
+    }
+    if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+        return false;
+    }
+    return true;
+}
+
 app.get('/api/animals',(req, res) => {
     let results = animals;
     //req.query is multifaceted(combining multiple parameters)
@@ -66,6 +105,23 @@ app.get('./api/animals/:id',(req, res)=>{
         res.json(result);
     } else {
         res.send(404);
+    }
+});
+
+//two ways to post data:
+//1. The developer team populates the server with the data manually, because they have access to the server code.
+//2. Users of the app populate the server with data by sending data from the client side of the application to the server.
+app.post('/api/animals',(req,res)=>{
+    //req.body is where our incoming content will be
+    //set id based on what the next index of the array will be (but only will work if we dont remove any data from animals.json)
+    req.body.id = animals.length.toString();
+
+    if(!validateAnimal(req.body)){
+        res.status(400).send('The animal is not properly formatted');
+    } else {
+        //add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body,animals)
+        res.json(animal);
     }
 });
 
