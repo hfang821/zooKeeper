a0a21ad593b60e0fca6e31a4c70b3258762dfeb0
hfang821 <95199209+hfang821@users.noreply.github.com>
2022-05-03 18:55:35 -0400

    fixing path bugs

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,20 +2,20 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/',(req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepr-public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 router.get('/animals',(req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepr-public/animals.html'));
+    res.sendFile(path.join(__dirname, '../../public/animals.html'));
 })
 
 router.get('/zookeepers',(req, res)=>{
-    res.sendFile(path.join(__dirname, './public/zookeepr-public/zookeepers.html'))
+    res.sendFile(path.join(__dirname, '../../public/zookeepers.html'))
 })
 
 //the * will act as a wildcard and any route that wasn't defined will fall under this request.
 router.get('*',(req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepr-public/index.html'));
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 app.use(express.urlencoded({ extended:true}));
 //parse incoming JSON data into the req.body javascript object
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/api',apiRoutes);
 app.use('/',htmlRoutes);
-app.use(express.static('public'));
 
 /* moved to animals.js
 function filterByQuery(query,animalsArray){
